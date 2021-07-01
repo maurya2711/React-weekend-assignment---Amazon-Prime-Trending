0650d63ac0f633ab37fa6dfb8bfdf30d7ed9fdcc
Sharad Maurya <sharadmaurya4600@gmail.com>
2021-07-01 14:48:24 +0530

    second Build

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,101 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
+
 import "../styles/App.css";
 
-const App = (props) => {
-  console.log(props.slides);
-  const [current, setCurrent] = useState(0);
-  const prev = () => {
-    if (current > 0) {
-      setCurrent(current - 1);
-    } else {
-      setCurrent(0);
-    }
-  };
+ 
 
-  const next = () => {
-    current < props.slides.length - 1 && setCurrent(current + 1);
-  };
-  const restart = () => {
-    setCurrent(0);
-  };
-  return (
-    <>
-      <div className="App">
-        <div>
-          <h1 data-testid="title">{props.slides[current].title}</h1>
-          <p data-testid="text">{props.slides[current].text}</p>
-        </div>
-        <button
-          data-testid="button-prev"
-          disable={current === 0 ? true : false}
-          onClick={prev}
-        >
-          Previous
-        </button>
-        <button
-          data-testid="button-restart"
-          disable={current === props.slides.length - 1 ? true : false}
-          onClick={restart}
-        >
-          Restart
-        </button>
-        <button
-          data-testid="button-next"
-          disable={current === 0 ? true : false}
-          onClick={next}
-        >
-          Next
-        </button>
-      </div>
-    </>
-  );
+const App = (props) => {
+
+ console.log(props.slides);
+
+ const [currSlides, setCurrSlides] = useState(0);
+
+ 
+
+const clickPrev = () => {
+
+ currSlides > 0 && setCurrSlides(currSlides - 1);
+
+ };
+
+ 
+
+const clickNext = () => {
+
+ currSlides < props.slides.length - 1 && setCurrSlides(currSlides + 1);
+
+ };
+
+ 
+
+const clickRestart = () => {
+
+ setCurrSlides(0);
+
+ };
+
+ 
+
+return (
+
+ <div className="App">
+
+ <div className="carousel">
+
+ <h1 data-testid="title">{props.slides[currSlides].title}</h1>
+
+ <p data-testid="text">{props.slides[currSlides].text}</p>
+
+ </div>
+
+ <button
+
+ onClick={clickPrev}
+
+ disabled={currSlides === 0 ? true : false}
+
+ data-testid="button-prev"
+
+ >
+
+ Prev
+
+ </button>
+
+ <button
+
+ onClick={clickNext}
+
+ disabled={currSlides === props.slides.length - 1 ? true : false}
+
+ data-testid="button-next"
+
+ >
+
+ Next
+
+ </button>
+
+ <button
+
+ onClick={clickRestart}
+
+ disabled={currSlides === 0 ? true : false}
+
+ data-testid="button-restart"
+
+ >
+
+ Restart
+
+ </button>
+
+ </div>
+
+ );
+
 };
+
+ 
 
 export default App;
